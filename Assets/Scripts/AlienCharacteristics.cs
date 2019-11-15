2b0d3545a9e1f59d8da73d5baccadb0ec67d00e6
Taher <taherkamoun5@gmail.com>
2019-11-14 23:12:45 -0500

    Début barre de points de vie pour Alien

--- Assets/Scripts/AlienCharacteristics.cs
+++ Assets/Scripts/AlienCharacteristics.cs
@@ -2,50 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlienCharacteristics : MonoBehaviour
 {
-    public int lifePoints = 100;
-    private Animator alienAnimator;
+  public float MaxHealthPoint = 100;
+  private float currentHealth;
 
-    // Start is called before the first frame update
-    void Start()
+  private Animator alienAnimator;
+
+  public Slider healthbar;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    currentHealth = MaxHealthPoint;
+    healthbar.value = calculateHealth();
+
+    alienAnimator = this.transform.GetChild(0).GetComponent<Animator>();
+    alienAnimator.SetTrigger("Smash Attack");
+  }
+
+  private float calculateHealth()
+  {
+    return currentHealth / MaxHealthPoint;
+  }
+
+  /*private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag.Equals("projectile"))
     {
-        alienAnimator = this.transform.GetChild(0).GetComponent<Animator>();
-        alienAnimator.SetTrigger("Smash Attack");
+      this.currentHealth--;
+      if (this.currentHealth <= 0)
+      {
+        alienAnimator.SetTrigger("Die");
+        Destroy(this.gameObject.GetComponent<BoxCollider>());
+      }
     }
+  }*/
 
-    private void OnCollisionEnter(Collision collision)
+  private IEnumerator DieAfterAnimation(float duree)
+  {
+    yield return new WaitForSeconds(duree - 0.15f);
+    Destroy(this.gameObject);
+  }
+
+  public void TakeDamage(int damages)
+  {
+    if (currentHealth > 0)
     {
-        if (collision.gameObject.tag.Equals("Projectile"))
-        {
-            TakeDamage(collision.gameObject.GetComponent<BulletController>().bulletDamages);
-        }
+      currentHealth -= damages;
+      calculateHealth();
+      if (currentHealth < 0) currentHealth = 0;
     }
-
-    private IEnumerator DieAfterAnimation(float duree)
+    if (currentHealth == 0) // if lifePoints <= 0
     {
-        yield return new WaitForSeconds(duree - 0.15f);
-        Destroy(this.gameObject);
+      alienAnimator.SetTrigger("Die");
+      Destroy(this.gameObject.GetComponent<BoxCollider>());
+
+      var animController = alienAnimator.runtimeAnimatorController;
+      var clip = animController.animationClips.First(a => a.name == "Die");
+
+      StartCoroutine(DieAfterAnimation(clip.length));
     }
-
-    public void TakeDamage(int damages)
-    {
-        if (lifePoints > 0)
-        {
-            lifePoints -= damages;
-            if (lifePoints < 0) lifePoints = 0;
-        }
-        else // if lifePoints <= 0
-        {
-            alienAnimator.SetTrigger("Die");
-            Destroy(this.gameObject.GetComponent<BoxCollider>());
-
-            var animController = alienAnimator.runtimeAnimatorController;
-            var clip = animController.animationClips.First(a => a.name == "Die");
-
-            StartCoroutine(DieAfterAnimation(clip.length));
-        }
-        //TODO faire apparaitre la barre de vie des aliens
-    }
+    //TODO faire apparaitre la barre de vie des aliens
+  }
 }
